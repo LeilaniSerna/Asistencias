31782375ae840d34004392e5213cd8241a06fa7c
LeilaniSerna <leilanisernagomez81@gmail.com>
2025-11-12 09:09:14 -0600

    Base de datos actualizada

--- backend/db_connection.py
+++ backend/db_connection.py
@@ -1,12 +1,12 @@
 import mysql.connector
 
-# Conexion a la Nube (Railway)
+# Conexion a la Nube (Railway - Cuenta Principal)
 DB_CONFIG = {
-    "host": "metro.proxy.rlwy.net",
+    "host": "nozomi.proxy.rlwy.net",
     "user": "root",
-    "password": "psAiVQzBxzmSWKkfbNTeDVaMWNdVejzN",
-    "database": "checador_db",  # Usamos esta porque tu script creó las tablas aquí
-    "port": 39030               # ¡Importante! El puerto debe ser un número (sin comillas)
+    "password": "mrFldMyxhidgpyEHEFcRFTFLWLFzThjP",
+    "database": "checador_db",  # Mantenemos esto porque tu script .sql crea las tablas aquí
+    "port": 41728               # Nuevo puerto actualizado
 }
 
 def obtener_conexion():
@@ -17,13 +17,3 @@ def obtener_conexion():
         print(f"Error al conectar a la base de datos: {err}")
         raise err
 
-# Prueba de conexión (Opcional: Puedes comentar esto después de probar)
-if __name__ == "__main__":
-    try:
-        conexion = obtener_conexion()
-        if conexion.is_connected():
-            print("✅ ¡Conexión exitosa a la base de datos en la nube!")
-            print(f"Conectado a: {DB_CONFIG['host']}")
-        conexion.close()
-    except Exception as e:
-        print("❌ Falló la conexión:", e)
